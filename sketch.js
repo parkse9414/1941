5368c3975bf7aa3bd81be721619c7e0bf86f69e3
박세원 <parkse9414@naver.com>
2014-11-20 13:53:31 +0900

    a

--- sketch.js
+++ sketch.js
@@ -4,7 +4,13 @@ var count;
 var count2;
 var count3;
 
+var score=0;
 
+var bn=100;
+var bID = 0;
+var c=[];
+var bX=[];
+var bY=[];
 function setup()
 {
     createCanvas(800,900);
@@ -17,11 +23,14 @@ function setup()
     count=0;
     count2=0;
     count3=0;
-
+    for(var  i = 0 ; i < bn ; i++){
+        c[i]=0;
+    }
 }
 
 function draw()
 {
+    background(0);
 	background(img3);
 
 
@@ -54,8 +63,43 @@ function draw()
      if(count3>900){
     count3=0;
     }
+    fill(255,0,0);
+    ellipse(300,count3*0.8,200,200);
+
     word();
     word2();
+    
+    fill(255,255,0);
+    for(var i = 0 ; i < bn ; i++){
+
+        if(c[i]>0){
+            c[i]+=5;
+            ellipse(bX[i],bY[i]-c[i],5,10);
+            if(dist(30,count,bX[i],bY[i]-c[i])<100){
+                count =-500;
+                c[i]=0;
+                score +=20;
+            }
+            if(dist(600,count2*2,bX[i],bY[i]-c[i])<100){
+                count2 =-500;
+                c[i]=0;
+                score +=30;
+            }
+            if(dist(300,count3*0.8,bX[i],bY[i]-c[i])<100){
+
+                count3 =-500;
+                c[i]=0;
+score +=120;
+
+            }
+
+        }
+        if(bY[i]-c[i]<0){
+            c[i]=0;
+        }
+    }
+
+    console.log("Score:"+score);
 }
 
 
@@ -70,16 +114,35 @@ function drawBairplane2(x,y) {
 function drawBairplane3(x,y) {
     image(img6,x,y);
 }
+
 function word(){
     fill(0);
     textSize(36);
     textFont("Helvetica");
     text("1P  48400", 10, 40);
-
 }
+
 function word2(){
     fill(0);
     textSize(36);
     textFont("Helvetica");
     text("CREDIT:8", 600, 870);
+}
+
+function mouseClicked(){
+
+    bX[bID]=mouseX;
+    bY[bID]=mouseY-70;
+    c[bID]=1;
+    bID++;
+    if(bID >= bn){
+        bID = 0;
+    }
+
+}
+
+function dist(bx,by,ex,ey){
+    var d;
+    d = sqrt(pow((bx-ex),2)+pow((by-ey),2));
+    return d;
 }
