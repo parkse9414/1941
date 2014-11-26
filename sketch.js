5dbb43121b202e5fe4e50adc6b52d43454b07b21
박세원 <parkse9414@naver.com>
2014-11-26 19:04:48 +0900

    a

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function draw()
     /*background(0);
 
     background(img3);*/
-    drawBackground(400,count4*4);
+    drawBackground(400,count4);
     count4=count4+1;
     if(count4>900){
         count4=0;
